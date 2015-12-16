8d1030a11df3df7f4625f7ebc5326de7ff2fe5e6
Henry <henryma14@gmail.com>
2015-12-15 21:08:39 -0800

    Modified

--- ChangeGridviewRowBkColor/Default.aspx.cs
+++ ChangeGridviewRowBkColor/Default.aspx.cs
@@ -15,17 +15,20 @@ namespace ChangeGridviewRowBkColor
         {
             try {
                 DataAccess da = new DataAccess();
+                da.OpenConnection();
                 int rows = Convert.ToInt32(ConfigurationManager.AppSettings["Rows"]);
                 DataSet ds = da.GetData(rows);
                 GridView1.DataSource = ds;
                 GridView1.DataBind();
                 GridView1.Columns[2].Visible = false; // hide this column but its value accessible 
+                da.CloseConnection();
             }
             catch (Exception ex)
             {
                 ErrorMessage.Text = ex.Message;
-                ErrorMessage.Visible = true;
+                ErrorMessage.Visible = true;                
             }
+            
         }
 
         protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
